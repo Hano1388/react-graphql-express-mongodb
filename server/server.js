13e95a4cb03b619d8b945ccdf787e9472edf56d1
Hano <hinsul3113@gmail.com>
2019-07-10 14:09:29 -0700

    added ability to create and show details of each song and create and list lyrics

--- server/server.js
+++ server/server.js
@@ -10,19 +10,27 @@ const app = express();
 // Replace with your mongoLab URI
 const MONGO_URI = 'mongodb://stephen:stephen@ds021182.mlab.com:21182/lyricaldb';
 if (!MONGO_URI) {
-  throw new Error('You must provide a MongoLab URI');
+	throw new Error('You must provide a MongoLab URI');
 }
 
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGO_URI);
 mongoose.connection
-  .once('open', () => console.log('Connected to MongoLab instance.'))
-  .on('error', error => console.log('Error connecting to MongoLab:', error));
+	.once('open', () => console.log('Connected to MongoLab instance.'))
+	.on('error', error => console.log('Error connecting to MongoLab:', error));
 
 app.use(bodyParser.json());
-app.use('/graphql', expressGraphQL({
-  schema,
-  graphiql: true
-}));
+app.use(
+	'/graphql',
+	expressGraphQL({
+		schema,
+		graphiql: true,
+	})
+);
+
+const webpackMiddleware = require('webpack-dev-middleware');
+const webpack = require('webpack');
+const webpackConfig = require('../webpack.config.js');
+app.use(webpackMiddleware(webpack(webpackConfig)));
 
 module.exports = app;
